6ad09397f22b64b6b388f34b0be558680e4a64f6
Junhyuk Lee <ljhjoon00@gmail.com>
2022-02-06 16:17:19 +0900

    init : Upload code from react master class

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "0464036979fa3be658654f0ec0c7d2e3";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minumum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
